aec008e2eaa0e68652fca0e6af5d98436b0dc75f
Will Toll <will@jtoll.com>
2020-06-25 15:06:00 -0500

    Additional features and greater support
    - Added support for ModifiableTestableWorlds in AirValidator
    - Added a SafelistValidator that provides a simple safelist test

--- src/main/java/com/terraformersmc/shapes/impl/validator/AirValidator.java
+++ src/main/java/com/terraformersmc/shapes/impl/validator/AirValidator.java
@@ -2,7 +2,10 @@ package com.terraformersmc.shapes.impl.validator;
 
 import com.terraformersmc.shapes.api.validator.AllMeetValidator;
 import com.terraformersmc.shapes.api.Position;
+import net.minecraft.world.ModifiableWorld;
+import net.minecraft.world.TestableWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
 /**
  * @author <Wtoll> Will Toll on 2020-06-07
@@ -10,18 +13,35 @@ import net.minecraft.world.World;
  */
 public class AirValidator extends AllMeetValidator {
 
-    private final World world;
+    private final WorldView worldView;
+    private final TestableWorld testableWorld;
 
-    public AirValidator(World world) {
-        this.world = world;
+    public AirValidator(WorldView world) {
+        this.worldView = world;
+        this.testableWorld = null;
     }
 
-    public static AirValidator of(World world) {
+    public AirValidator(TestableWorld world) {
+        this.worldView = null;
+        this.testableWorld = world;
+    }
+
+    public static AirValidator of(WorldView world) {
+        return new AirValidator(world);
+    }
+
+    public static AirValidator of(TestableWorld world) {
         return new AirValidator(world);
     }
 
     @Override
     public boolean test(Position position) {
-        return world.isAir(position.toBlockPos());
+        if (worldView != null) {
+            return worldView.isAir(position.toBlockPos());
+        } else if (testableWorld != null) {
+            return testableWorld.testBlockState(position.toBlockPos(), (state) -> state.isAir());
+        } else {
+            return false;
+        }
     }
 }
